2e6c197d85a33127865d49ca6e1c968033ef7d35
Hasley <raudineac101@gmail.com>
2024-07-15 22:06:58 -0400

    Add create action to Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,20 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(allowed_article_parameters)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def allowed_article_parameters
+    params.require(:article).permit(:title, :body)
+  end
 end
