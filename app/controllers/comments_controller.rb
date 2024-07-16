936b8eb2c6100baff2db827dbf386eb0ce8164bb
Hasley <raudineac101@gmail.com>
2024-07-15 22:45:24 -0400

    Add create action to Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(allowed_comment_parameters)
+
+    redirect_to @article
+  end
+
+  private
+
+  def allowed_comment_parameters
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
